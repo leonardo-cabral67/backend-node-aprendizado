6b88a92600b6f148ad729428a8dbce2e8a82da6e
Leonardo Cabral <leonardolccabral@gmail.com>
2022-11-01 19:54:10 -0300

    feat: util getFutureDate function created, tested and implemented.

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,12 +1,10 @@
 import { test, expect } from "vitest";
+import { getFutureDate } from "../tests/utils/get-future-date";
 import { Appointment } from "./appointment";
 
 test("create an appointment", () => {
-  const startsAt = new Date();
-  const endsAt = new Date();
-
-  startsAt.setDate(startsAt.getDate() + 1);
-  endsAt.setDate(endsAt.getDate() + 2);
+  const startsAt = getFutureDate("2022-08-10");
+  const endsAt = getFutureDate("2022-08-11");
 
   const appointment = new Appointment({
     customer: "John Doe",
@@ -19,11 +17,8 @@ test("create an appointment", () => {
 });
 
 test("Should not be possible create a earlier end date compared to start date", () => {
-  const startsAt = new Date();
-  const endsAt = new Date();
-
-  endsAt.setDate(endsAt.getDate() + 1);
-  endsAt.setDate(endsAt.getDate() - 1);
+  const startsAt = getFutureDate("2022-08-10");
+  const endsAt = getFutureDate("2022-08-09");
 
   expect(() => {
     return new Appointment({
